52a7fc918aef6088715494f4b81963136f29ffd6
ticaustelian <ticaustelian@gmail.com>
2023-11-28 12:35:04 +0200

    Added Chess.hs

---
+++ src/Chess.hs
@@ -0,0 +1,15 @@
+module Chess where
+
+import Test.HUnit
+import Test.QuickCheck
+
+type Board = [[Square]]
+
+type Square = Maybe Piece
+
+data Piece = Piece PColor PType
+data PColor = White | Black
+data PType = Pawn | Knight | Bishop | Rook | Queen | King
+
+showPiece :: Piece -> Char
+showPiece 
